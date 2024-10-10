21464db604501eb7b315451fda9611da6751ef8f
context-dependent <tomas.k.mcmanus@gmail.com>
2024-10-10 16:59:43 -0400

    load_qx_surveys etc.

---
+++ R/extract.R
@@ -0,0 +1,25 @@
+save_survey <- function(qx_survey_id) {
+  d <- bpqx::fetch_responses(qx_survey_id)
+  cache_file <- new_cache_file(qx_survey_id)
+  readr::write_rds(d, cache_file)
+}
+
+load_survey <- function(qx_survey_id, fetch = FALSE, date = "latest") {
+  cache_file <- find_cache_file(qx_survey_id, date)
+  
+  if (is.null(cache_file) || fetch) {
+    save_survey(qx_survey_id)
+    return(readr::read_rds(find_cache_file(qx_survey_id, date = "latest")))
+  } else {
+    return(readr::read_rds(cache_file))
+  }
+}
+
+load_qx_surveys <- function(fetch = FALSE, date = "latest") {
+  survey_info <- readr::read_rds("data/survey_info.rds")
+  survey_info |> 
+    mutate(data = purrr::map(id, ~load_survey(.x, fetch = fetch, date = date))) 
+}
+
+
+
---
+++ R/zzz.R
@@ -0,0 +1,36 @@
+z_path <- function(...) {
+  file.path("Z:/WCG Inclusion Group Research", ...)
+}
+
+find_cache_file <- function(obj_id, date = "latest", extension = ".rds") {
+  cache_dir <- z_path("surveys", "cache", obj_id)
+
+  if(!fs::dir_exists(cache_dir)) {
+    return(NULL)
+  }
+
+  cache_files <- cache_dir |> 
+    fs::dir_ls() |> 
+    sort()
+
+  if (length(cache_files) == 0) {
+    return(NULL)
+  }
+  
+  if (date != "latest") {
+    cache_files <- grep(strftime(lubridate::ymd(date), formate = "%Y%m%d"), cache_files, value = TRUE)
+  }
+  return(dplyr::last(cache_files))
+} 
+
+new_cache_file <- function(obj_id, extension = ".rds") {
+  obj_dir <- z_path("surveys", "cache", obj_id)
+  if(!fs::dir_exists(obj_dir)) {
+    fs::dir_create(obj_dir, recurse = TRUE)
+  }
+  
+  file.path(
+    obj_dir, 
+    glue::glue("{strftime(lubridate::now(), format = '%Y%m%d%H%M%S')}{extension}")
+  )
+}
